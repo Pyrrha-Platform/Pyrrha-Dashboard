8fb622bd0df952360b6b90320f836a74b9275435
Daniel Krook <krook@us.ibm.com>
2020-11-22 16:55:11 -0500

    Remove the field renaming functions.

--- prometeo-dashboard/src/hooks/useDashboard.js
+++ prometeo-dashboard/src/hooks/useDashboard.js
@@ -6,41 +6,10 @@ const client = async (url, options) => {
   return data;
 };
 
-const clone = (obj) => Object.assign({}, obj);
-
-const renameKey = (object, key, newKey) => {
-  const clonedObj = clone(object);
-  const targetKey = clonedObj[key];
-  delete clonedObj[key];
-  clonedObj[newKey] = targetKey;
-  return clonedObj;
-};
-
-const transformData = (firefighters) => {
-  let transformedFirefighters = [];
-
-  firefighters.forEach((firefighter) => {
-    firefighter = renameKey(firefighter, "firefighter_id", "firefighterId");
-    firefighter = renameKey(firefighter, "device_id", "deviceId");
-    firefighter = renameKey(firefighter, "timestamp_mins", "timestampMins");
-    firefighter = renameKey(firefighter, "carbon_monoxide", "carbonMonoxide");
-    firefighter = renameKey(firefighter, "nitrogen_dioxide", "nitrogenDioxide");
-    firefighter.firefighterCode = "GRAF7";
-    firefighter.firefighterFirst = "Joan";
-    firefighter.firefighterLast = "Herrera";
-    firefighter.firefighterEmail = "graf7@graf.cat";
-    firefighter.increment = "10 min avg";
-    transformedFirefighters.push(firefighter);
-  });
-
-  return transformedFirefighters;
-};
-
 const fetchDashboard = async () => {
   try {
     const data = await client(`/api/v1/dashboard-now`);
     console.log(data);
-    //return transformData(data.firefighters);
     return data.firefighters;
   } catch (e) {
     console.log(e);
