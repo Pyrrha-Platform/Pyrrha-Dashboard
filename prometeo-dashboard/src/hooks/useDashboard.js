3fbbb0dcaaeb71677b54c23743fe666061c37fd3
Daniel Krook <krook@us.ibm.com>
2020-11-24 17:35:44 -0500

    Completed dashboard update logic.

--- prometeo-dashboard/src/hooks/useDashboard.js
+++ prometeo-dashboard/src/hooks/useDashboard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import Utils from "../utils/Utils";
 
 const client = async (url, options) => {
   const response = await fetch(url, options);
@@ -10,37 +11,73 @@ const fetchDashboard = async () => {
   try {
     const data = await client(`/api/v1/dashboard-now`);
     console.log(data);
-    return data.firefighters;
+    return data.firefighters.sort((a, b) =>
+      a.firefighterId > b.firefighterId ? 1 : -1
+    );
   } catch (e) {
     console.log(e);
   }
 };
 
 const updateDashboard = (dashboard, message) => {
-  // Clone the existing dashboard array
   console.log("dashboard", dashboard);
   let newDashboard = JSON.parse(JSON.stringify(dashboard));
   console.log("newDashboard", newDashboard);
 
-  // Fetch the new message
   let newMessage = JSON.parse(message);
 
   console.log(typeof newMessage, newMessage);
   if (typeof newMessage === "object") {
     if (newMessage instanceof Array) {
+      // For each item in the newDashboard.current array, check to see if
+      // there's a replacement in the newMessage array, then replace
       console.log("array", newMessage);
-      // Merge array, replacing existing members with the same id
-      // TODO, for now just replacing
-      newDashboard.current = newMessage;
+      newDashboard.current.forEach((oldReading) => {
+        newMessage.forEach((newReading) => {
+          if (oldReading.firefighterId == newReading.firefighterId) {
+            console.log(
+              "Replacing an old reading with a new one in the array",
+              newMessage
+            );
+            newDashboard.current = Utils.arrayRemove(
+              newDashboard.current,
+              oldReading
+            );
+            newDashboard.current.push(newReading);
+          }
+        });
+      });
+      // Naive replacement
+      // newDashboard.current = newMessage;
     } else {
+      // It's a single firefighterupdate, replace the
+      // latest reading for the firefighter, or add it
       console.log("object", newMessage);
-      // Replace or add a member with this object
-      // TODO pull out the individual object
+      let matchedOldReading = false;
+      newDashboard.current.forEach((oldReading) => {
+        if (oldReading.firefighterId == newMessage.firefighterId) {
+          console.log(
+            "Replacing a single old reading with a new one",
+            newMessage
+          );
+          newDashboard.current = Utils.arrayRemove(
+            newDashboard.current,
+            oldReading
+          );
+          newDashboard.current.push(newMessage);
+          matchedOldReading = true;
+        }
+      });
+      if (!matchedOldReading) {
+        console.log("Adding a new reading", newMessage);
+        newDashboard.current.push(newMessage);
+      }
       console.log(newDashboard);
-      newDashboard.current.push(newMessage);
     }
   }
-  return newDashboard.current;
+  return newDashboard.current.sort((a, b) =>
+    a.firefighterId > b.firefighterId ? 1 : -1
+  );
 };
 
 const useDashboard = () => {
