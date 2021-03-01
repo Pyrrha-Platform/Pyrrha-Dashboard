d9cd8aab58c398d79aa2aa2be4a3a7e3d5a526f1
Daniel Krook <krook@us.ibm.com>
2021-03-01 11:03:06 -0500

    Removed unnecessary meta files.

--- prometeo-dashboard/src/components/SideMenu/index.js
+++ prometeo-dashboard/src/components/SideMenu/index.js
@@ -1,2 +1,28 @@
-import SideMenu from './SideMenu';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  SideNav,
+  SideNavItems,
+  SideNavMenu,
+  SideNavMenuItem,
+} from 'carbon-components-react/lib/components/UIShell';
+import { Settings32 } from '@carbon/icons-react';
+
+function SideMenu() {
+  return (
+    <SideNav isRail isChildOfHeader={false} aria-label="Side navigation">
+      <SideNavItems>
+        <SideNavMenu renderIcon={Settings32} title="Settings" large>
+          <SideNavMenuItem element={Link} to="/language">
+            Language
+          </SideNavMenuItem>
+          <SideNavMenuItem element={Link} to="/profile">
+            Profile
+          </SideNavMenuItem>
+        </SideNavMenu>
+      </SideNavItems>
+    </SideNav>
+  );
+}
+
 export default SideMenu;
