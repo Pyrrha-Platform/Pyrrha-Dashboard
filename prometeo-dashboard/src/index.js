2e45f3123dfc922bece7b65fe9a337ebe4ded793
Daniel Krook <krook@us.ibm.com>
2021-03-08 01:02:10 -0500

    Testing signed commits.
    Signed-off-by: Daniel Krook <krook@us.ibm.com>

--- prometeo-dashboard/src/index.js
+++ prometeo-dashboard/src/index.js
@@ -8,12 +8,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-  
 );
 
 serviceWorker.unregister();
