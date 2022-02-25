9afae8b69428e4ae3beecf344f47b982d0f61691
Charlie Evans <charlie.evans@ibm.com>
2022-02-25 13:52:03 -0600

    Correct error on check in useDashboard

--- pyrrha-dashboard/src/hooks/useDashboard.js
+++ pyrrha-dashboard/src/hooks/useDashboard.js
@@ -28,8 +28,9 @@ const updateDashboard = (dashboard, message) => {
   let newDashboard = { current: [] };
   if (
     dashboard !== undefined &&
-    dashboard.length !== 0 &&
-    dashboard.map !== undefined
+    dashboard.current !== undefined &&
+    dashboard.current !== null &&
+    dashboard.current.length !== 0
   ) {
     newDashboard = JSON.parse(JSON.stringify(dashboard));
   }
